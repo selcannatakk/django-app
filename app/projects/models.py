6b4cae38d81e09e4966a33062b3c607dd795696b
selcanatak2629 <192803025@ogr.cbu.edu.tr>
2023-10-22 23:37:00 +0300

    admin and models

--- app/projects/models.py
+++ app/projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Project(models.Model):
+    id = models.URLField(default=uuid.uuid4, unique=True,
+                         primary_key=True, editable=False)
+    # owner =
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # featured_image =
+    demo_link = models.CharField(max_length=1000, null=True, blank=True)
+    source_link = models.CharField(max_length=1000, null=True, blank=True)
+    vote_total = models.IntegerField(default=0)
+    vote_ratio = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
