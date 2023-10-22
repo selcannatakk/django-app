0662a4d538233ee1c456059bc530e15ab3dccfde
selcanatak2629 <192803025@ogr.cbu.edu.tr>
2023-10-23 02:47:50 +0300

    Database Relationships

--- app/projects/models.py
+++ app/projects/models.py
@@ -2,6 +2,16 @@ from django.db import models
 import uuid
 
 
+class Tag(models.Model):
+    id = models.URLField(default=uuid.uuid4, unique=True,
+                         primary_key=True, editable=False)
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Project(models.Model):
     id = models.URLField(default=uuid.uuid4, unique=True,
                          primary_key=True, editable=False)
@@ -13,7 +23,30 @@ class Project(models.Model):
     source_link = models.CharField(max_length=1000, null=True, blank=True)
     vote_total = models.IntegerField(default=0)
     vote_ratio = models.IntegerField(default=0)
+    tags = models.ManyToManyField('Tag', blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'up'),
+        ('down', 'down'),
+    )
+
+    id = models.URLField(default=uuid.uuid4, unique=True,
+                         primary_key=True, editable=False)
+    # owner =
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=50, choices=VOTE_TYPE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.value
+
+
