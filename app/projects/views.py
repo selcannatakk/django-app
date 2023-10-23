53b30be253741111e68ec814d34b73bd4ff95715
selcanatak2629 <192803025@ogr.cbu.edu.tr>
2023-10-23 03:38:01 +0300

    refactor

--- app/projects/views.py
+++ app/projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
 projectsList = [
     {
@@ -24,15 +25,18 @@ projectsList = [
 
 
 def projects(request):
-    context = {'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    projectObject = None
-    for i in projectsList:
-        if i['id'] == str(pk):
-            projectObject = i
-
-    context = {'project': projectObject}
+    projectObject = Project.objects.get(id=pk)
+    tags = projectObject.tags.all()
+    reviews = projectObject.review_set.all()
+    context = {
+        'project': projectObject,
+        'tags': tags,
+        'reviews': reviews
+    }
     return render(request, 'projects/single-project.html', context)
